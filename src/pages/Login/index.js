5dc14ab01010f7547c07d3b085508bf64dfcb42b
Thanh tran <trantienthanh2412@gmail.com>
2023-03-30 07:51:10 +0700

    add btn gg auth

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -113,6 +113,12 @@ export default function LoginPage() {
     }
   };
 
+  const onSignIn = (googleUser) => {
+    const id_token = googleUser.getAuthResponse().id_token;
+
+    console.log("id token:", id_token);
+  };
+
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
   };
@@ -235,6 +241,25 @@ export default function LoginPage() {
             >
               Đăng nhập
             </Button>
+            <div
+              id="g_id_onload"
+              data-client_id="897567384173-rqc5id46ujmvf8es4fhla2ptt96i6v50.apps.googleusercontent.com"
+              data-context="signin"
+              data-ux_mode="popup"
+              data-login_uri="http://localhost:24508/api/user/login-google"
+              data-auto_prompt="false"
+            ></div>
+
+            <div
+              class="g_id_signin"
+              data-type="standard"
+              data-shape="rectangular"
+              data-theme="outline"
+              data-text="signin_with"
+              data-size="large"
+              data-logo_alignment="left"
+            ></div>
+
             <Grid container spacing={2}>
               <Grid item xs={12} sm>
                 <Link href={Paths.resetPassword} variant="body2">
